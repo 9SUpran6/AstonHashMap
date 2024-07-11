8fa2dc0c67de93321a93216c9ba9cea287c6242c
9SUpran6 <56307702+9SUpran6@users.noreply.github.com>
2024-07-11 19:36:34 +0300

    Update Main.java
    added generics

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 //Прежде всего реализуем интерфейс Map с помощью команды "implements"
-public class Main implements Map{
+public class Main<K,V> implements Map<K, V>{
 
 //Вводим константы первичного размера массива и коэффициента заполнения
 //При котором массив будет увеличиваться вдвое
@@ -13,15 +11,15 @@ public class Main implements Map{
     private static final double LOAD_FACTOR = 0.75;
 
 //Создаем первичный массив с size = 0, т.к. там не лежит ни одного элемента
-    private Entry[] array = new Entry[INITIAL_CAPACITY];
+    private Object[] array = new Object[INITIAL_CAPACITY];
     private int size = 0;
 
 //Переписываем все методы интерфейса Map
-    //Первый метод Clear создает новый массив, а старый удалит сборщик муссора,
-    //т.к. на него бошльше не будет ссылки
+    //Первый метод Clear создает новый массив, а старый удалит сборщик мусора,
+    //т.к. на него больше не будет ссылки
     @Override
     public void clear() {
-        array = new Entry[INITIAL_CAPACITY];
+        array = new Object[INITIAL_CAPACITY];
         size = 0;
     }
 
@@ -31,7 +29,7 @@ public class Main implements Map{
         //Для начала нужно получить позицию элемента в массиве
         int position = getElementPosition(key, array.length);
         //После этого объявляем что нужный нам элемент на этой позиции
-        Entry existedElement = array[position];
+        Entry existedElement = (Entry) array[position];
         //Если значение на этой позиции не null, а ключи совпадают
         //то переписываем ссылки и уменьшаем размер на 1
         if (existedElement != null && existedElement.key.equals(key)){
@@ -65,8 +63,8 @@ public class Main implements Map{
     @Override
     public List<V> valuesList() {
         List<V> result = new ArrayList<>();
-        for (Entry entry : array){
-            Entry existedElement = entry;
+        for (Object entry : array){
+            Entry existedElement = (Entry) entry;
             while (existedElement != null){
                 result.add(existedElement.value);
                 existedElement = existedElement.next;
@@ -78,8 +76,8 @@ public class Main implements Map{
     @Override
     public Set<K> keySet() {
         Set<K> result = new HashSet<>();
-        for (Entry entry : array){
-            Entry existedElement = entry;
+        for (Object entry : array){
+            Entry existedElement = (Entry) entry;
             while (existedElement != null){
                 result.add(existedElement.key);
                 existedElement = existedElement.next;
@@ -93,7 +91,7 @@ public class Main implements Map{
     //Для начала получаем номер позиции, затем присваиваем элементу
     //Значение по этой позиции и после проверки на equals() выдаем значение
         int position = getElementPosition(key, array.length);
-        Entry existedElement = array[position];
+        Entry existedElement = (Entry) array[position];
         while (existedElement != null){
             if (existedElement.key.equals(key)){
                 return existedElement.value;
@@ -115,10 +113,10 @@ public class Main implements Map{
         }
     }
     //Метод для того, чтобы положить элемент в ячейку массива
-    private boolean put(K key, V value, Entry[] dst) {
-    //Так же получаем позицию элмента
+    private boolean put(K key, V value, Object[] dst) {
+    //Так же получаем позицию элемента
         int position = getElementPosition(key, dst.length);
-        Entry existedElement = dst[position];
+        Entry existedElement = (Entry) dst[position];
     //Проверяем на null. Если null, то кладем в него элемент, где
     //Следующая ссылка null.
         if (existedElement == null) {
@@ -145,11 +143,11 @@ public class Main implements Map{
 
     //Метод увеличения массива в 2 раза
     private void increaseArray() {
-        Entry[] newArray = new Entry[array.length * 2];
+        Object[] newArray = new Object[array.length * 2];
     //Копируем элементы из существующего массива в newArray,
     //А после увеличения присваиваем array = newArray
-        for (Entry entry : array) {
-            Entry existedElement = entry;
+        for (Object entry : array) {
+            Entry existedElement = (Entry) entry;
             while (existedElement != null) {
                 put(existedElement.key, existedElement.value, newArray);
                 existedElement = existedElement.next;
@@ -159,14 +157,14 @@ public class Main implements Map{
     }
 
 
-    //Метод получения позиции элмента
+    //Метод получения позиции элемента
     private int getElementPosition(K key, int arrayLenght){
         return Math.abs(key.hashCode() % arrayLenght);
     }
 
     //Класс нода, ссылки, Entry, кому как удобнее, в которой
     //хранится ключ, значение, ссылка на следующий элемент
-    private static class Entry {
+    private class Entry {
         private K key;
         private V value;
         private Entry next;
