2fa27d5ea53c6b9de8744cf6534f8fdfd155f668
9SUpran6 <56307702+9SUpran6@users.noreply.github.com>
2024-07-11 19:43:33 +0300

    Update MapTest.java
    added generics

--- src/test/java/org/example/MapTest.java
+++ src/test/java/org/example/MapTest.java
@@ -5,20 +5,21 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
 public class MapTest {
-    private Map map;
+    private Map<Key, Value> map;
 
     @Before
     public void setUp() throws Exception {
-        map = new Main();
+        map = new Main<>();
     }
 
     @Test
     //Проверка, что если вставить 100 элементов - их будет 100
     public void WhenPut100ElementsSizeBecome100() {
         for (int i =0; i < 100; i++){
-            K key = new K(i);
-            V value = new V("value " + i);
+            Key key = new Key(i);
+            Value value = new Value("value " + i);
             map.put(key, value);
         }
         assertEquals(100, map.size());
@@ -28,8 +29,8 @@ public class MapTest {
     public void WhenPut10DifferentKeysThenSize10() {
         for (int i = 0; i < 100; i++){
             int id = i % 10;
-            K key = new K(id);
-            V value = new V("value " + id);
+            Key key = new Key(id);
+            Value value = new Value("value " + id);
             map.put(key, value);
         }
         assertEquals(10, map.size());
@@ -39,8 +40,8 @@ public class MapTest {
     //Проверка, что количество ключей совпадает с количеством значений
     public void countOfKeysMustBeEqualsCountOfValues() {
         for (int i = 0; i < 100; i++) {
-            K key = new K(i);
-            V value = new V("value " + i);
+            Key key = new Key(i);
+            Value value = new Value("value " + i);
             map.put(key, value);
         }
         assertEquals(100, map.size());
@@ -52,8 +53,8 @@ public class MapTest {
     //Проверка на очистку массива
     public void Clear() {
         for (int i = 0; i < 100; i++) {
-            K key = new K(i);
-            V value = new V("Value " + i);
+            Key key = new Key(i);
+            Value value = new Value("Value " + i);
             map.put(key, value);
         }
         map.clear();
@@ -64,14 +65,14 @@ public class MapTest {
     //Проверка, что мы сможем достать правильное значение по ключу
     public void methodGetMustReturnRightValue() {
         for (int i = 0; i < 100; i++) {
-            K key = new K(i);
-            V value = new V("Value " + i);
+            Key key = new Key(i);
+            Value value = new Value("Value " + i);
             map.put(key, value);
         }
-        K key = new K(50);
-        V value = map.get(key);
-        String expectedCarBrand = "Value 50";
-        assertEquals(expectedCarBrand, value.getValue());
+        Key key = new Key(50);
+        Value value = map.get(key);
+        String expectedValue = "Value 50";
+        assertEquals(expectedValue, value.getValue());
     }
 
 
@@ -79,12 +80,12 @@ public class MapTest {
     //Проверка, что один и тот же элемент будет удален только 1 раз
     public void removeReturnTrueOnly() {
         for (int i = 0; i < 100; i++) {
-            K key = new K(i);
-            V value = new V("Value " + i);
+            Key key = new Key(i);
+            Value value = new Value("Value " + i);
             map.put(key, value);
         }
         assertEquals(100, map.size());
-        K removingElement = new K(16);
+        Key removingElement = new Key(16);
         assertTrue(map.remove(removingElement));
         assertEquals(99, map.size());
         assertFalse(map.remove(removingElement));
